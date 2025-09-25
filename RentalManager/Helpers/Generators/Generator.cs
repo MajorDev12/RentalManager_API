1957a0b46e16bfedd6d76a92b5b1e47dfde2d7c0
Major_Developer <majordev12@gmail.com>
2025-09-25 23:37:44 +0300

    AddedGeneratorClass

---
+++ RentalManager/Helpers/Generators/Generator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace RentalManager.Helpers.Generators
+{
+    public class Generator
+    {
+        private readonly Random _random = new Random();
+
+        public string InvoiceNumberGenerator()
+        {
+            string prefix = "INV";
+            string datePart = DateTime.Now.ToString("yyyyMMdd");
+            int randomPart = _random.Next(1000, 9999);           
+
+            return $"{prefix}-{datePart}-{randomPart}";
+        }
+    }
+}
